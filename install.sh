a4e90204009d49faf27a541206a8211b94b32c5f
Sascha Wiesenthal <sascha.wiesenthal@inotec-licht.de>
2018-12-04 16:10:40 +0100

    install script added

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+TMUX_LOC=~/
+TMUX_CONF=.tmux.conf
+
+if [ -f "$TMUX_LOC/$TMUX_CONF" ]; then
+	echo "file exists"
+	read -p "do you want to override it (y/n)? " answer
+	if [ $answer != 'y' ]; then
+		exit 1
+	fi
+fi
+
+echo "copy file $TMUX_CONF to your home directory."
+cp $TMUX_CONF $TMUX_LOC
+exit 0
+
