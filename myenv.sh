10c937c4e74e43242e1d2ef8c38df68dfc187829
Sascha Wiesenthal <sascha.wiesenthal@inotec-licht.de>
2019-09-02 14:07:40 +0200

    environment further expanded

--- myenv.sh
+++ myenv.sh
@@ -46,6 +46,12 @@ tmux send-keys "docker run --rm -it --net=host -v $DOC_DIR:/documents/ asciidoct
 tmux send-keys "asciidoctor -r asciidoctor-diagram main.adoc"
 #tmux send-keys "asciidoctor -a stylesheet=colony.css -a stylesdir=../stylesheets -r asciidoctor-diagram main.adoc"
 
+tmux selectp -t 0
+tmux send-keys ":NERDTree" C-m
+
+##############################################################################
+# new window doc-todo
+##############################################################################
 DOC_DIR=~/Dokumente/asciidoc/doc-todo/
 tmux new-window -t $SESSION -n DOC-TODO
 tmux send-keys "cd $DOC_DIR" C-m
@@ -60,6 +66,27 @@ tmux send-keys "cd $DOC_DIR" C-m
 tmux send-keys "docker run --rm -it --net=host -v $DOC_DIR:/documents/ asciidoctor/docker-asciidoctor" C-m
 tmux send-keys "./gen.sh"
 #tmux send-keys "asciidoctor -a stylesheet=colony.css -a stylesdir=../stylesheets -r asciidoctor-diagram main.adoc"
+tmux selectp -t 0
+tmux send-keys ":NERDTree" C-m
+
+##############################################################################
+# new window gui development
+##############################################################################
+GUI_DIR=~/sourcen/tftGui/
+tmux new-window -t $SESSION -n GUI
+tmux send-keys "cd $GUI_DIR" C-m
+tmux send-keys "git status" C-m
+
+##############################################################################
+# new window compile gui for phytec platform
+##############################################################################
+PHY_DIR=~/sourcen/mydockerfiles/phy-env/
+tmux new-window -t $SESSION -n PHY
+tmux send-keys "cd $PHY_DIR" C-m
+tmux send-keys "./03_compile.sh"
+
+### select window as default
+tmux selectw -t 3
 
 tmux -2 attach-session -t $SESSION
 
